fb1c9b674e2a127e07523107ce115ae15259455d
Hank Lee <hank@140-113-89-234.Dorm9.NCTU.edu.tw>
2016-12-03 19:51:50 +0800

    update

--- creenshot_test.py
+++ creenshot_test.py
@@ -1,15 +1,24 @@
 #-- include('examples/showgrabfullscreen.py') --#
 import pyscreenshot as ImageGrab
 import time
+from image_client import send_image
 from datetime import datetime
-
+import base64
 
 while(1):
-	current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+	#filename = "./screenshot/"
+	current_time = "test.jpeg"
+	#current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+".jpeg"
 	print(current_time)
-	with open(current_time, 'wb') as f:
-		im=ImageGrab.grab()
-		im.save(f, format='png')
-	
+	im=ImageGrab.grab().save(current_time,'JPEG')
+	#im = ImageGrab.grab().tobytes()
+	with open("test.jpeg", 'rb') as image_file:
+		image = image_file.read()
+	print(send_image('140.113.89.75','9999',image))
+
 	time.sleep(10)
+
+
+
+
 
