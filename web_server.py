6db37c759573ba55386ef854e6d75a1b180be2c7
Hank Lee <hank@140-113-89-234.Dorm9.NCTU.edu.tw>
2016-12-09 17:09:07 +0800

    update web server

---
+++ web_server.py
@@ -0,0 +1,34 @@
+import tornado.ioloop
+import tornado.web
+import base64
+import json
+
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		
+		"""
+		message = "Yooooo!"
+		with open("test.jpeg", "rb") as f:
+			img = base64.b64encode(f.read())
+			myres = {"photo":str(img),"msg":message}
+			myres = json.dumps(myres)
+			self.write(myres)
+		"""
+		with open('data.json') as data_file:    
+			data = json.load(data_file)
+			myres = {"photo":data["photo"],"msg":data["msg"]}
+			myres = json.dumps(myres)
+			self.write(myres)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+	app = make_app()
+	app.listen(8080) 
+	print('server running: 0.0.0.0:8888')
+	tornado.ioloop.IOLoop.current().start()
