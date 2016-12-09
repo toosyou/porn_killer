0e4ab94af6c2678105830a90590280668597f155
Hank Lee <hank@140-113-89-234.Dorm9.NCTU.edu.tw>
2016-12-09 17:21:28 +0800

    local screenshot update

---
+++ local_screenshot.py
@@ -0,0 +1,24 @@
+#-- include('examples/showgrabfullscreen.py') --#
+import pyscreenshot as ImageGrab
+import time
+from image_client import send_image
+from datetime import datetime
+import base64
+
+while(1):
+	#filename = "./screenshot/"
+	file_name = "current_img.jpeg"
+	current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+	print(current_time)
+	im=ImageGrab.grab().save(file_name,'JPEG')
+	#im = ImageGrab.grab().tobytes()
+	with open("current_img.jpeg", 'rb') as image_file:
+		image = image_file.read()
+	#print(send_image('140.113.89.75','9999',image))
+
+	time.sleep(10)
+
+
+
+
+
