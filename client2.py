ccf5f23a0e8671be8b72dde22fbb346633ef8d1a
Hank Lee <hank@140-113-89-234.Dorm9.NCTU.edu.tw>
2016-12-09 13:14:40 +0800

    web_server update

---
+++ client2.py
@@ -0,0 +1,15 @@
+import requests
+import base64
+import json
+
+r = requests.get('http://0.0.0.0:8888').text
+
+
+myres = json.loads(r)
+print(myres["msg"])
+
+g = open("out.jpg", "wb")
+photo = base64.b64decode(myres["photo"][1:-1])
+g.write(photo)
+g.close()
+
