2b7ef7d6648e309ce26a454152aed596dc6e3a9c
Hank's Mac <hankhplee@gmail.com>
2016-12-23 18:02:39 +0800

    update local_creenshot

--- local_screenshot.py
+++ local_screenshot.py
@@ -10,11 +10,11 @@ while(1):
 	file_name = "current_img.jpeg"
 	current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 	print(current_time)
-	im=ImageGrab.grab().save(file_name,'JPEG')
+	#im=ImageGrab.grab().save(file_name,'JPEG')
 	#im = ImageGrab.grab().tobytes()
 	with open("current_img.jpeg", 'rb') as image_file:
 		image = image_file.read()
-	#print(send_image('140.113.89.75','9999',image))
+	print(send_image('mip1070.toosyou.nctu.me','9999',image))
 
 	time.sleep(10)
 
