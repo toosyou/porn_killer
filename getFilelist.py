80819b5391db9bef6ba6950d699d61cc0eef53f4
dimension4TW <hankhplee@gmail.com>
2016-12-24 01:10:22 +0800

    update

--- getFilelist.py
+++ getFilelist.py
@@ -9,17 +9,19 @@ dirs = os.listdir( path )
 dict = {}
 
 # This would print all the files and directories
-for file in reversed(dirs):
-	if(file[-4:] == "json"):
-		MACaddr = file[-20:-5]
-		time = file[0:14]
-		if(MACaddr in dict):
-			if(dict[MACaddr] < time):
+
+while(1):
+	for file in reversed(dirs):
+		if(file[-4:] == "json"):
+			MACaddr = file[-20:-5]
+			time = file[0:14]
+			if(MACaddr in dict):
+				if(dict[MACaddr] < time):
+					dict[MACaddr] = time
+			else:
 				dict[MACaddr] = time
-		else:
-			dict[MACaddr] = time
 
-data = json.dumps(dict)
-r = requests.post('http://0.0.0.0:8888',data)
-
+	data = json.dumps(dict)
+	r = requests.post('http://0.0.0.0:8888',data)
+	sleep(1)
 
